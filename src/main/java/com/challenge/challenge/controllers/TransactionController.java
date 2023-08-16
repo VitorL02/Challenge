3345df88a9416c236e03f6c621640f52f329074c
Vitor Lucas <vitorcrispim@yahoo.com.br>
2023-08-16 14:54:32 -0300

    Adicionando o Readme juntamente com o banco docker

--- src/main/java/com/challenge/challenge/controllers/TransactionController.java
+++ src/main/java/com/challenge/challenge/controllers/TransactionController.java
@@ -3,6 +3,8 @@ package com.challenge.challenge.controllers;
 
 import com.challenge.challenge.dtos.TransactionDTO;
 import com.challenge.challenge.services.TransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,10 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/transactions")
 public class TransactionController {
 
+    public static final String TRANSACTION_DESCRIPTION = "Transações - Cadastro e Auditoria";
+    public static final String TRANSACTIONS = "Transações";
+
+    public static final String REGISTRY_TRANSACTION_DESC = "Realiza uma transação entre usuarios";
+    public static final String  REGISTRY_TRANSACTION_SUM = "Realiza uma transação entre usuarios caso o perfil deles seja USER (Os perfis MERCHANTS podem somente receber transações)";
     @Autowired
    private TransactionService transactionService;
 
     @PostMapping
+    @Tag(description = TRANSACTION_DESCRIPTION,name = TRANSACTIONS)
+    @Operation(summary = REGISTRY_TRANSACTION_DESC,description = REGISTRY_TRANSACTION_SUM )
     public ResponseEntity transactionSave(@RequestBody @Valid TransactionDTO transactionDTO) throws  Exception{
         var transaction = transactionService.transactionSave(transactionDTO);
         return  ResponseEntity.ok(transaction);
